b5a8c4743a6501a94d8e856688872c851df7d4bf
Влдаимир Курусь <vladimir.kurus@yandex.ru>
2022-06-17 22:39:07 +0700

    (Система) Добавил moment.js

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,6 +6,9 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import moment from "moment";
+
+moment.locale('ru');
 
 Vue.config.productionTip = false
 
