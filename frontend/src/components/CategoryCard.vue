67e9c595899c4adda7d2d61878e429600a81189d
Влдаимир Курусь <vladimir.kurus@yandex.ru>
2022-06-20 22:10:27 +0700

    (Карточки) Добавил тень

--- frontend/src/components/CategoryCard.vue
+++ frontend/src/components/CategoryCard.vue
@@ -75,6 +75,7 @@ export default class CategoryCard extends Vue {
   aspect-ratio: 1.5;
   overflow: hidden;
   cursor: pointer;
+  filter: drop-shadow(0px 0px 2px rgba(0, 0, 0, 0.12)) drop-shadow(0px 2px 2px rgba(0, 0, 0, 0.24));
 
   .dc__background {
     height: 100%;
--- frontend/src/components/DishCard.vue
+++ frontend/src/components/DishCard.vue
@@ -70,6 +70,7 @@ export default class DishCard extends Vue {
   aspect-ratio: 1.5;
   overflow: hidden;
   cursor: pointer;
+  filter: drop-shadow(0px 0px 2px rgba(0, 0, 0, 0.12)) drop-shadow(0px 2px 2px rgba(0, 0, 0, 0.24));
 
   .dc__name {
     position: absolute;
